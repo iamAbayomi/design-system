11553bc7708046471f2015bfc477165bd2e07371
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-06 22:31:38 +0100

    feature: added inputfield components and stories

--- src/components/InputField/InputField.stories.tsx
+++ src/components/InputField/InputField.stories.tsx
@@ -1,1 +1,9 @@
-export {}
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import InputField from "./InputField";
+
+export default {
+    title: 'InputField',
+    component: InputField
+} as ComponentMeta <typeof InputField >
+
+export const primary : ComponentStory <typeof InputField> = () => <InputField type={"name"} placeholder={"same"} />
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,1 +1,37 @@
-export {}
+import { ChangeEventHandler } from "react";
+import styled from "styled-components";
+
+type Props ={
+    type: string,
+    placeholder: string,
+    value?: string,
+    onChange?: ChangeEventHandler<HTMLInputElement>
+}
+
+
+const InputField: React.FC<Props> = (Props) => {
+    return(
+        <div>
+            <EditField
+                type={Props.type}
+                placeholder={Props.placeholder}
+                value={Props.value}
+                onChange={Props.onChange}
+            />
+        </div>
+    )
+}
+
+export default InputField;
+
+
+const EditField = styled.input `
+    width: 300px;
+    height: 44px;
+    display: block;
+    padding-left: 30px;
+    margin-top: 10px;
+    margin-bottom: 20px ;
+    border: 2px solid #BDBDBD;
+    border-radius: 10px;
+`
