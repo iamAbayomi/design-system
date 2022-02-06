0c9b5b213a4d3ebc227226ba5be14f4b5aa0d85b
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-06 10:00:19 +0100

    feature: changed table columns and data

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <PercentageOverviewCard percentageText={'No of users'} percentage={10}/>
-      <Table tableColums={["data","data"]} tableData={[["daad", "eeq"]]} />
+      <Table tableColums={["Name","Role"]} tableData={[["John", "Admin"]]} />
     </div>
   );
 }
