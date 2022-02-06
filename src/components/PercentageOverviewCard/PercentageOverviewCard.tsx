9d68f261e2167afa7687583deb53532d715b68ce
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-06 04:50:38 +0100

    install @types/styled-components

---
+++ src/components/PercentageOverviewCard/PercentageOverviewCard.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import '../pages/Overview.css'
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import styled from 'styled-components'
+
+type props = {
+    percentageText : string,
+    percentage : number
+}
+
+const PercentageOverviewCard: React.FC<props> = (props) => {
+    return(
+        <div>
+            <div className="single-transaction-card">
+                {/* <img className="transaction-options" src="/vectors/options-menu.svg" /> */}
+                <TransactionGroup className="">
+                    <CircularProgressGroup>
+                        {/* <div style={{ width:100, height:100}}> */}
+                        <div className="circle-progress">
+                            <CircularProgressbar 
+                                value={props.percentage} 
+                                text={`${props.percentageText}`} 
+                                styles={buildStyles({pathColor : '#010066', textColor: '#010066' } )}/>
+                        </div>
+                    </CircularProgressGroup> 
+                    <p className="transaction-text">{props.percentageText}</p>
+                </TransactionGroup>
+            </div>
+        </div>
+    )
+}
+
+
+const TransactionGroup = styled.div `
+    display:flex;   
+    clear:both;
+`
+
+
+
+const CircularProgressGroup = styled.div `
+
+`
