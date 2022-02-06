8222115714c75342af4f4c5c088af9d996b02a93
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-06 22:03:38 +0100

    feature: added the changes to the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Button from './components/Button/Button';
 import Chips from './components/Chips/Chips';
+import DropDown from './components/Dropdown/DropDown';
 import PercentageOverviewCard from './components/OverviewCard/OverviewCard';
 import Table from './components/Table/Table';
 
@@ -12,6 +13,7 @@ function App() {
       <Table tableColums={["Name","Role"]} tableData={[["John", "Admin"]]} />
       <Button width={'132px'} height={'42px'} color={'blue'} margin={'20px 0px'} backgroundColor={'white'} buttonText={'Secondary'} />
       <Chips chipsText={'successful'} />
+      <DropDown menuTitle={"work"} menuItem={['place', 'house']} />
     </div>
   );
 }
--- src/components/Dropdown/DropDown.tsx
+++ src/components/Dropdown/DropDown.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Dropdown } from 'react-bootstrap'
+import '../../Dashboard.css'
 
 type props = {
     menuTitle: string,
