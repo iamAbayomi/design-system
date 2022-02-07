79f8eb6ba1e685c871ea8805826342e651dfda32
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-07 14:43:48 +0100

    feature: added parameters to preview.js

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,15 @@
+import { addParameters } from '@storybook/react';
+import { DocsPage, DocsContainer } from '@storybook/addon-docs';
+
+addParameters({
+  docs: {
+    container: DocsContainer,
+    page: DocsPage,
+  },
+});
+
+
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +19,4 @@ export const parameters = {
     },
   },
 }
+
